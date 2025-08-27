df22da8da6a0ce3e75660679ae4b13bb7adec4e0
Avinash <129788500+babloo004@users.noreply.github.com>
2025-08-27 21:38:44 +0530

    Update securityGroup.tf

--- modules/netowrk/securityGroup.tf
+++ modules/netowrk/securityGroup.tf
@@ -15,6 +15,19 @@ resource "azurerm_network_security_group" "aznsg01" {
     destination_address_prefix = "*"
   }
 
+  security_rule {
+    name                       = "jenkins"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+    
+
   security_rule  {
     name = "allow-all-outbound"
     priority = 100
@@ -46,6 +59,19 @@ resource "azurerm_network_security_group" "aznsg02" {
         source_address_prefix = "*"
         destination_address_prefix = "*"
     }
+
+    security_rule {
+    name                       = "jenkins"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+    }
+    
 
     security_rule {
         name = "allow-all-outbound"
